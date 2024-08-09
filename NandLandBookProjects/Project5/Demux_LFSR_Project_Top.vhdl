e70de445d90042ad4fdb53b0e10e187f031495ac
Ryan Leahy <rpleahy12@gmail.com>
2024-08-09 13:24:20 -0700

    Project 5: Reorganized the files to include two top files. One uses a counter to implement the blinking, another uses an LFSR.
    Chapter 6 of the book also discusses RAM and gives an implementation that has been added to this project.
    
    It is important to note that the RAM is not used to implement the project. It was just included in the same chapter as the project.
    
    In Chapter 6, mux/demuxes, shift registers, RAM, and FIFO's were covered.

--- NandLandBookProjects/Project5/Demux_LFSR_Project_Top.vhdl
+++ NandLandBookProjects/Project5/Demux_LFSR_Project_Top.vhdl
@@ -1,3 +1,5 @@
+-- Project 5 top level that uses an LFSR (Linear feedback shift register) to implement the blinking.
+-- This is the original top level, the counter top level is an iteration.
 library ieee;
 use ieee.std_logic_1164.all;
 
@@ -14,43 +16,30 @@ entity Demux_LFSR_Project_Top is
 end entity Demux_LFSR_Project_Top;
 
 
--- All commented out code uses the LFSR instead of a counter for toggling
 architecture RTL of Demux_LFSR_Project_Top is
-    --signal r_LFSR_Toggle : std_logic := '0';
-    --signal w_LFSR_Done   : std_logic;
-    constant COUNT_LIMIT      : integer := 4194303;
-    signal   w_Counter_Toggle : std_logic;
+    signal r_LFSR_Toggle : std_logic := '0';
+    signal w_LFSR_Done   : std_logic;
 
 begin
-    --LFSR_22 : entity work.LFSR_22
-    --port map (
-        --i_Clk       => i_Clk,
-        --o_LFSR_Data => open, -- unconnected (we don't need the data, just the done signal)
-        --o_LFSR_Done => w_LFSR_Done
-    --);
-
-    --process (i_Clk) is
-    --begin
-        --if rising_edge(i_Clk) then
-            --if w_LFSR_Done = '1' then
-                --r_LFSR_Toggle <= not r_LFSR_Toggle;
-            --end if;
-        --end if;
-    --end process;
-    Toggle_Counter : entity work.Count_And_Toggle
-    generic map (
-        COUNT_LIMIT => COUNT_LIMIT
-    )
+    LFSR_22 : entity work.LFSR_22
     port map (
-        i_Clk    => i_Clk,
-        i_Enable => '1',
-        o_Toggle => w_Counter_Toggle
+        i_Clk       => i_Clk,
+        o_LFSR_Data => open, -- unconnected (we don't need the data, just the done signal)
+        o_LFSR_Done => w_LFSR_Done
     );
+
+    process (i_Clk) is
+    begin
+        if rising_edge(i_Clk) then
+            if w_LFSR_Done = '1' then
+                r_LFSR_Toggle <= not r_LFSR_Toggle;
+            end if;
+        end if;
+    end process;
 
     Demux_Inst : entity work.Demux_1_To_4
     port map (
-        --i_Data  => r_LFSR_Toggle,
-        i_Data  => w_Counter_Toggle,
+        i_Data  => r_LFSR_Toggle,
         i_Sel0  => i_Switch_1,
         i_Sel1  => i_Switch_2,
         o_Data0 => o_LED_1,
